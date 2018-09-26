dd28994bb6f1c88c62f4da2938f105a666c7175f
X00136708 <X00136708@computing.stu.it-tallaght.ie>
2018-09-26 13:54:47 +0100

    Lab1

---
+++ Lab1/a/DontDie.cpp
@@ -0,0 +1,18 @@
+#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	double personWeight
+	double sweetener = .001;
+	int cansToKillMouse = 2;
+	double weightMouse = .5;
+	double murderStats = weightMouse / sweetener
+		cout << "Enter the weight you will be: "
+		cin >> personWeight;
+
+	double murderPerson = murderStats * personWeight;
+	cout << murderPerson;
+
+}
