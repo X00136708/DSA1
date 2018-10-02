00f0f423a7b34e202d681a58be5b6bd12fa153cf
X00136708 <32193220+X00136708@users.noreply.github.com>
2018-10-02 11:11:14 +0100

    Add files via upload

--- Lab1/a/DontDie.cpp
+++ Lab1/a/DontDie.cpp
@@ -1,18 +1,17 @@
-#include "pch.h"
 #include <iostream>
 
 using namespace std;
 
 int main() {
-	double personWeight
+	double personWeight;
 	double sweetener = .001;
-	int cansToKillMouse = 2;
+	int cansToKillMouse = 15;
 	double weightMouse = .5;
-	double murderStats = weightMouse / sweetener
-		cout << "Enter the weight you will be: "
+	double murderStats = weightMouse / (sweetener*cansToKillMouse);
+	cout << "Enter the weight you will be: ";
 		cin >> personWeight;
 
 	double murderPerson = murderStats * personWeight;
-	cout << murderPerson;
+	cout << "It will take " << murderPerson << " cans to kill you.";
 
 }
